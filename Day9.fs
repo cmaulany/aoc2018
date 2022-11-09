d00c733b01be5180e8b946fdc90aca3375d152b6
Christian Maulany <cmaulany@gmail.com>
2022-11-10 00:18:24 +0100

    Day 9 part 2

--- Day9.fs
+++ Day9.fs
@@ -1,96 +1,92 @@
 module Day9
 
-type Marble = int
+type Node(value: int) as this =
+    member val Previous = this with get,set
+    member val Next = this with get,set
+    
+    member val Value = value
 
-type Player = {
-    Score: int
-}
-
-type State = {
-    Circle: Marble list
-    Current: Marble
-    Players: Player list
-}
-
-let day9 = 
-
-    let playerCount = 441
-
-    let initialState = {
-        Circle = [0]
-        Current = 0
-        Players = [ 
-            for _ in 1..playerCount -> 
-            {
-                Score = 0
-            }
-        ]
-    }
-
-    let finalState = 
-        (initialState, seq { 1 .. 71032 })
-        ||> Seq.fold (fun (state: State) i -> 
-            let current = state.Current
-            let next = i
-
-            let currentIndex =
-                state.Circle
-                |> Seq.findIndex (fun marble -> marble = current) 
-
-            let circleSize = Seq.length state.Circle
-
-            if next % 23 = 0 then
-                let sevenCcIndex = (currentIndex - 7 + circleSize) % circleSize 
-                let sevenCc = 
-                    state.Circle
-                    |> Seq.item sevenCcIndex
-
-
-                let newCircle =
-                    state.Circle
-                    |> List.except [sevenCc; next]
-
-                let newPosition = sevenCcIndex % (circleSize - 1)
-
-                let newCurrent = 
-                    newCircle
-                    |> Seq.item newPosition
-
-                let score = sevenCc + next
-                let playerIndex = (i - 1) % (Seq.length state.Players)
-
-                let player =
-                    state.Players
-                    |> Seq.item playerIndex
-
-                let players = 
-                    state.Players
-                    |> List.updateAt playerIndex { Score = player.Score + score}
-
-                { state with
-                    Circle = newCircle
-                    Current = newCurrent
-                    Players = players
-                }
-            else 
-                let newPosition = (currentIndex + 2) % circleSize 
-                
-                let nextCircle = 
-                    state.Circle
-                    |> List.insertAt newPosition next
-
-                // printfn "%d - %A (%d)" current nextCircle next
-                { state with
-                    Circle = nextCircle
-                    Current = next
-                }
+    member this.Rotate (n) =
+        (this, { 1 .. abs n})
+        ||> Seq.fold (fun node _ -> 
+            if n > 0 
+                then node.Next 
+                else node.Previous
         )
 
-    printfn "--Day 9--"
+    member this.Insert (value) = 
+        let newNode = new Node(value)
+        let previous = this.Previous
+        previous.Next <- newNode
+        newNode.Previous <- previous
+        newNode.Next <- this
+        this.Previous <- newNode
+        newNode
 
-    let winner = 
-        finalState.Players
-        |> Seq.map (fun player -> player.Score)
+    member this.Remove () =
+        let previous = this.Previous
+        let next = this.Next
+        previous.Next <- next
+        next.Previous <- previous
+        next
+
+    member this.Collect () =
+        let mutable result = [this]
+        let mutable node = this.Previous
+        while node <> this do
+            result <- node::result
+            node <- node.Previous
+        result
+        |> List.map (fun n -> n.Value)
+        
+type GameState = {
+    Circle: Node
+    PlayerCount: int
+    Scores: Map<int, int64>
+}
+
+let node = new Node(0)
+
+let day9 = 
+    let run playerCount marbleCount = 
+        let state = {
+            Circle = Node(0)
+            PlayerCount = playerCount
+            Scores = Map.empty
+        }
+
+        (state, { 1..marbleCount })
+        ||> Seq.fold (fun state i -> 
+            if i % 23 = 0 then
+                let toTake = state.Circle.Rotate(-7)
+
+                let playerId = (i % state.PlayerCount) - 1
+                let previousScore =
+                    match state.Scores |> Map.tryFind playerId with
+                    | None -> int64 0
+                    | Some(score) -> score
+                let newScore = previousScore + int64 toTake.Value + int64 i
+                let scores = state.Scores |> Map.add playerId newScore
+
+                { state with
+                    Circle = toTake.Remove() 
+                    Scores = scores }
+            else
+                { state with 
+                    Circle = state.Circle.Rotate(2).Insert(i) }
+        )
+
+    let gamePart1 = run 441 71032
+    let gamePart2 = run 441 (71032 * 100)
+
+    let maxScore state = 
+        state.Scores
+        |> Map.values
         |> Seq.max
 
-    printfn "Answer part 1: %d" winner
+    let resultPart1 = maxScore gamePart1
+    let resultPart2 = maxScore gamePart2
+
+    printfn "--Day 9--"
+    printfn "Result part 1: %d" resultPart1
+    printfn "Result part 2: %d" resultPart2
